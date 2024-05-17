6a2914066cbf1e4fcd9346b83bc138047251ac06
dombaizoltan <dombaiz@kkszki.hu>
2024-05-17 10:20:06 +0200

    Több pizza hozzárendelése, egyébb update

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 let totalPrice = 0;
 
-function addToCart(pizzaName, price) {
+function updateTotal() {
+}
+
+function addToCart(name, price) {
     const cartItems = document.getElementById("cart-items");
-    const li = document.createElement("li");
-    li.textContent = pizzaName + " - $" + price;
-    cartItems.appendChild(li);
+    const item = document.createElement("div");
+    item.textContent = `${name} - $${price}`;
+    cartItems.appendChild(item);
     totalPrice += price;
     updateTotal();
 }
 
-
-function updateTotal() {
-    const totalElement = document.getElementById("total");
-    totalElement.textContent = "Összesen: $" + totalPrice;
-}
+fetch("https://pizza.kando-dev.eu/Pizza")
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (datas) {
+        console.log(datas);
+        let content = document.getElementById("pizza-menu");
+        for (const data of datas) {
+            let price = Math.floor(Math.random() * (20 - 8 + 1)) + 8;
+            content.innerHTML += `      
+            <div class="pizza">
+                <img src="${data.kepURL}" alt="${data.name}">
+                <h3>${data.name}</h3>
+                <p class="price">Ár: $${price}</p>
+                <button onclick="addToCart('${data.name}', ${price})">Kosárba</button>
+            </div>`;
+        }
+    });
 
 function order() {
     const cartItems = document.getElementById("cart-items");
+    const orderMessage = document.getElementById("order-message");
+    
     if (cartItems.children.length === 0) {
         alert("A kosár üres!");
     } else {
-        alert("Köszönjük a rendelést! A szállítás hamarosan megérkezik. Összesen: $" + totalPrice);
+        orderMessage.textContent = "Köszönjük a rendelést! A szállítás hamarosan megérkezik. Összesen: $" + totalPrice;
+        orderMessage.classList.remove("hidden");
+        orderMessage.classList.add("visible");
+
+        setTimeout(function() {
+            orderMessage.classList.remove("visible");
+            orderMessage.classList.add("hidden");
+        }, 5000);
+
         cartItems.innerHTML = "";
         totalPrice = 0;
         updateTotal();
